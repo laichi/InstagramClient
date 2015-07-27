07bf1fa2cfa2b81501ea72ae8763cc87cc5b50ca
Wayne Lai <laichi@doublepinkie-lm.tpcity.corp.yahoo.com>
2015-07-27 16:07:36 +0800

    init

---
+++ app/src/main/java/com/example/laichi/instagramclient/InstagramPhoto.java
@@ -0,0 +1,12 @@
+package com.example.laichi.instagramclient;
+
+/**
+ * Created by laichi on 7/27/15.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+}
---
+++ app/src/main/java/com/example/laichi/instagramclient/PhotosActivity.java
@@ -0,0 +1,96 @@
+package com.example.laichi.instagramclient;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class PhotosActivity extends ActionBarActivity {
+
+    public static final String CLIENT_ID = "349eae1ba71c4e52be30c96ea3abcf32";
+    private ArrayList<InstagramPhoto> photos;
+    private InstagramPhotosAdapter aPhotos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos);
+
+        photos = new ArrayList<>();
+
+        aPhotos = new InstagramPhotosAdapter(this, photos);
+
+        ListView lvPhotos = (ListView) findViewById(R.id.lvPhotos);
+        lvPhotos.setAdapter(aPhotos);
+        fetchPopularPhotos();
+    }
+
+    public void fetchPopularPhotos(){
+        String url = "https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=" + CLIENT_ID;
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(url, null, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                JSONArray photosJSON = null;
+                try{
+                    photosJSON = response.getJSONArray("data");
+                    for(int i = 0; i < photosJSON.length(); i++){
+                        JSONObject photoJson = photosJSON.getJSONObject(i);
+                        InstagramPhoto photo = new InstagramPhoto();
+                        photo.username = photoJson.getJSONObject("user").getString("username");
+                        photo.caption = photoJson.getJSONObject("caption").getString("text");
+                        photo.imageUrl= photoJson.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.imageHeight = photoJson.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                        photo.likesCount = photoJson.getJSONObject("likes").getInt("count");
+
+                        photos.add(photo);
+                    }
+                }
+                catch (JSONException e){
+                    e.printStackTrace();
+                }
+
+                aPhotos.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_photos, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
